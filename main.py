9caa358f4815f26bc430744e45607618108b18e4
nhdimitrov23 <nhdimitrov23@codingburgas.bg>
2026-04-21 15:59:36 +0300

    Add error 404 handling

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+    @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/login')
