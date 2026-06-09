60c8e7a6bd215097f7aeb9f4d286b2fb041a05bb
nhdimitrov23 <nhdimitrov23@codingburgas.bg>
2026-06-09 13:05:40 +0300

    Fix error in python file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
 def login():
     return render_template('login.html')
 @app.route('/register')
-def signup():
+def register():
     return render_template('register.html')
 
 if __name__ == '__main__':
